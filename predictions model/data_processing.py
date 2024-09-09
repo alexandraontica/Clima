6a2a6ec619910a733f459482f28431c9979df0ea
Alexandra Onțică <150956540+alexandraontica@users.noreply.github.com>
2024-09-09 03:53:24 +0300

    added model training

---
+++ predictions model/data_processing.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+def load_and_clean_data(file):
+    df = pd.read_csv(file, parse_dates=["Formatted Date"])
+
+    # drop duplicates for the same date and time
+    df = df.drop_duplicates(["Formatted Date"], keep="first")
+
+    # sort data by date
+    df.sort_values(by=["Formatted Date"], inplace=True)
+
+    # reset indexes (when removing duplicates, the indexes don't change)
+    df.reset_index(inplace=True, drop=True)
+
+    # drop unnecessary columns (loud cover has 0 values, the date doesn't play a role in our predictions)
+    df = df.drop(columns=["Loud Cover", "Formatted Date"])
+
+    # drop rows where precip type is null
+    df = df[df["Precip Type"].notna()]
+
+    # handle outliers
+    df = df[df["Humidity"] != 0.0]
+    df = df[df["Wind Speed (km/h)"] <= 60]
+    df = df[df["Pressure (millibars)"] > 0]
+    df.reset_index(inplace=True, drop=True)
+
+    return df
